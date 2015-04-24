79cb5480c6e1053ce12fdff196dcae935458b289
Kevin Murray <spam@kdmurray.id.au>
2015-04-24 12:03:46 +1000

    Optimise is_dimer function, add dimer file output

--- gbstrim.py
+++ gbstrim.py
@@ -26,10 +26,11 @@ import gzip
 SSW = skbio.alignment.StripedSmithWaterman
 
 CLI_ARGS = """
-USAGE: gbstrim -e RE_SITE <input_ILFQ>
+USAGE: gbstrim -e RE_SITE [-d DIMER_FILE]  <input_ILFQ>
 
 OPTIONS:
     -e RE_SITE      Restriction site sequence
+    -d DIMER_FILE   Keep dimers in file
 """
 
 LONGEST_BARCODE=8
@@ -57,7 +58,7 @@ def printrecs(r1, r2, stream=sys.stdout):
         print("+", file=stream)
         print("{quality}".format(**r), file=stream)
 
-def is_dimer(r1, r2, re_len):
+def is_dimer(r1, r2, re_len, dimer_file=None):
     """
     Checks if the start of the adaptor is in the first few bases of both reads.
     """
@@ -65,23 +66,22 @@ def is_dimer(r1, r2, re_len):
     r1seq = r1["sequence"]
     r2seq = r2["sequence"]
     align_until = re_len + ADAPTOR_LEN + LONGEST_BARCODE
-    r1aln = adapt(r1seq[re_len:align_until])
-    r2aln = adapt(r2seq[re_len:align_until])
-    r1len = len(r1aln.aligned_query_sequence)
-    r2len = len(r2aln.aligned_query_sequence)
-    if r1len != ADAPTOR_LEN or r2len != ADAPTOR_LEN:
+    aln = adapt(r1seq[re_len:align_until])
+    aln_len = len(aln.aligned_query_sequence)
+    if aln_len / float(ADAPTOR_LEN) < 0.9:
+        # adaptor alignment isn't complete
         return False
-    r1pos = r1aln.target_begin
-    r2pos = r2aln.target_begin
-    # If both alignments are within the alignment region, then we can only
-    # assume there's dimer or a stupidly small fragment.
-    if r1pos-re_len < align_until and r2pos-re_len < align_until:
-        # printrecs(r1, r2, stream=sys.stderr)
+    r1pos = aln.target_begin
+    if r1pos - re_len < align_until:
+        if dimer_file is not None:
+            printrecs(r1, r2, stream=dimer_file)
         return True
     return False
 
-def main_ilfq(ilfq_name, re_site):
+def main_ilfq(ilfq_name, re_site, dimer_file=None):
     pst1 = SSW(re_site)
+    if dimer_file is not None:
+        dimer_file = open(dimer_file, 'w')
     n_reads = 0
     n_adapt = 0
     n_trimmed = 0
@@ -99,7 +99,7 @@ def main_ilfq(ilfq_name, re_site):
         if r1aln.query_begin != 0 or r1aln.query_end != 4 or \
                 r2aln.query_begin != 0 or r2aln.query_end != 4:
             # Not read-through, but might be dimer.
-            if not is_dimer(r1, r2, re_len):
+            if not is_dimer(r1, r2, re_len, dimer_file):
                 printrecs(r1, r2)
             else:
                 n_adapt += 1
@@ -114,10 +114,12 @@ def main_ilfq(ilfq_name, re_site):
             print("Processed {:0.0f}K read pairs".format(n_reads /1000.0),
                   file=sys.stderr, end='\r')
     fh.close()
+    if dimer_file is not None:
+        dimer_file.close();
     print("Processed", n_reads, "read pairs", file=sys.stderr)
     print("Trimmed", n_trimmed, file=sys.stderr)
     print("Adaptor in", n_adapt, file=sys.stderr)
 
 if __name__ == "__main__":
     opts = docopt.docopt(CLI_ARGS)
-    main_ilfq(opts['<input_ILFQ>'], opts['-e'])
+    main_ilfq(opts['<input_ILFQ>'], opts['-e'], opts['-d'])
